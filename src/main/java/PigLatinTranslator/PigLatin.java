cdbca5755a26fdf00ca3b9f8e92faf58c9593b9a
Jiexiong <jiexiong@n148-p208.kthopen.kth.se>
2017-03-07 13:31:41 +0100

    test

--- src/main/java/PigLatinTranslator/PigLatin.java
+++ src/main/java/PigLatinTranslator/PigLatin.java
@@ -1,6 +1,29 @@
-package PigLatinTranslator;
+// package PigLatinTranslator;
+
+import java.util.Scanner;
 
 public class PigLatin {
+
+	public static boolean isConsonant(String word) {
+		boolean  b = word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("i") || word.startsWith("u");
+		return b;
+	}
+
+	public static void translatePigLatin(String read_word) {
+		
+		System.out.println(isConsonant(read_word));
+		System.out.println(read_word);
+
+
+	}
+
+    public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String read_word = sc.nextLine();
+		translatePigLatin(read_word);
+
+    }
 
 	//TODO implement piglatin translator following rule from https://en.wikipedia.org/wiki/Pig_Latin
 }
