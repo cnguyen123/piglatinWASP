f0f9ce49ee6b4fb8b21677a7eefc28d9a5841ab2
cnguyen123 <cnguyen111085@gmail.com>
2017-03-07 14:47:59 +0100

    fix typos in build.xml

--- src/main/java/PigLatinTranslator/PigLatin.java
+++ src/main/java/PigLatinTranslator/PigLatin.java
@@ -1,32 +1,8 @@
-import java.util.Scanner;
+package PigLatinTranslator;
 
 public class PigLatin {
 
-	public static boolean starts_with_vowel(String word) {
-		boolean  b = word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u");
-		return b;
-	}
+	//TODO implement piglatin translator following rule from https://en.wikipedia.org/wiki/Pig_Latin
 
-	public static String translatePigLatin(String read_word) {
-		if(starts_with_vowel(read_word))
-		{
-			read_word = read_word + "way";
-		}
-		else
-		{
-			read_word = read_word.substring(1, read_word.length()-1) + read_word.substring(0);
-			read_word = read_word + "ay";
-		}
-		return read_word;
-	}
 
-    public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Word to be translated:");
-		String read_word = sc.nextLine();
-
-		System.out.println("Translated in Pig Latin:");
-		System.out.println(translatePigLatin(read_word));
-    }
 }
