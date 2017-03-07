d43c78af7c7824c39e62b8f8869d83d03cd4b2e4
Jiexiong <jiexiong@n148-p208.kthopen.kth.se>
2017-03-07 17:13:19 +0100

    v0.15

--- src/main/java/PigLatinTranslator/PigLatin.java
+++ src/main/java/PigLatinTranslator/PigLatin.java
@@ -2,10 +2,11 @@ package PigLatinTranslator;
 import java.util.Scanner;
 
 public class PigLatin {
-
+	
 	public static boolean starts_with_vowel(String word) {
-		boolean  b = word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u");
-		return b;
+		boolean  b_low = word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u");
+		boolean  b_up = word.startsWith("A") || word.startsWith("E") || word.startsWith("I") || word.startsWith("O") || word.startsWith("U");
+		return b_low || b_up;
 	}
 
 	public static String translatePigLatin(String read_word) {
@@ -15,7 +16,7 @@ public class PigLatin {
 		}
 		else
 		{
-			read_word = read_word.substring(1, read_word.length()-1) + read_word.substring(0);
+			read_word = read_word.substring(1, read_word.length()) + read_word.toLowerCase().charAt(0);
 			read_word = read_word + "ay";
 		}
 		return read_word;
