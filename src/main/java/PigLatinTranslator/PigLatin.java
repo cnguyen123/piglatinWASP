e0da9a27a49c5c15ea943a2f7529637afbc24802
cnguyen123 <cnguyen111085@gmail.com>
2017-03-07 11:56:34 +0100

    First commit

---
+++ src/main/java/PigLatinTranslator/PigLatin.java
@@ -0,0 +1,6 @@
+package PigLatinTranslator;
+
+public class PigLatin {
+
+	//TODO implement piglatin translator following rule from https://en.wikipedia.org/wiki/Pig_Latin
+}
---
+++ src/test/java/Testing/PigLatinTest.java
@@ -0,0 +1,5 @@
+package Testing;
+
+public class PigLatinTest {
+//TODO implement testing for all methods from PigLatin
+}
