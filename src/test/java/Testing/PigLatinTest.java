8b5668fa74b039d22a722ffa66d44eccd8e41cf6
cnguyen123 <cnguyen111085@gmail.com>
2017-03-07 17:09:31 +0100

    implement test for method starts_with_vowel

--- src/test/java/Testing/PigLatinTest.java
+++ src/test/java/Testing/PigLatinTest.java
@@ -1,5 +1,16 @@
 package Testing;
-
+import org.junit.*;
+import static org.junit.Assert.*;
+import PigLatinTranslator.*;
 public class PigLatinTest {
 //TODO implement testing for all methods from PigLatin
+@Test
+public void starts_with_vowel(){
+	String inputString1 = "aGJKAgjaknbbagayoh";
+	String inputString2 = "helloabcd";
+	PigLatin pigLatin= new PigLatin();
+	assertTrue(pigLatin.starts_with_vowel(inputString1));
+	assertFalse(pigLatin.starts_with_vowel(inputString2));
+
+}
 }
