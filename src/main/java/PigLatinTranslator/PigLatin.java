24c12ef7a1f88f4be05c2ddbe9aea7b1113d26f7
Jiexiong <jiexiong@n148-p208.kthopen.kth.se>
2017-03-07 14:36:16 +0100

    v0.1

--- src/main/java/PigLatinTranslator/PigLatin.java
+++ src/main/java/PigLatinTranslator/PigLatin.java
@@ -1,37 +1,32 @@
-// package PigLatinTranslator;
-
 import java.util.Scanner;
 
 public class PigLatin {
 
-	public static boolean isConsonant(String word) {
+	public static boolean starts_with_vowel(String word) {
 		boolean  b = word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u");
 		return b;
 	}
 
-	public static void translatePigLatin(String read_word) {
-		
-
-		if isConsonant(read_word)
+	public static String translatePigLatin(String read_word) {
+		if(starts_with_vowel(read_word))
 		{
-
-			
+			read_word = read_word + "way";
 		}
-
-		System.out.println(isConsonant(read_word));
-
-		System.out.println(read_word);
-
-
+		else
+		{
+			read_word = read_word.substring(1, read_word.length()-1) + read_word.substring(0);
+			read_word = read_word + "ay";
+		}
+		return read_word;
 	}
 
     public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Word to be translated:");
 		String read_word = sc.nextLine();
-		translatePigLatin(read_word);
 
+		System.out.println("Translated in Pig Latin:");
+		System.out.println(translatePigLatin(read_word));
     }
-
-	//TODO implement piglatin translator following rule from https://en.wikipedia.org/wiki/Pig_Latin
 }
