e00fa17387dfb3a36471187b007dc599535999e6
Jiexiong <jiexiong@n163-p40.kthopen.kth.se>
2017-03-09 17:59:35 +0100

    -v0.22

--- src/main/java/PigLatinTranslator/PigLatin.java
+++ src/main/java/PigLatinTranslator/PigLatin.java
@@ -8,11 +8,19 @@ public class PigLatin {
 	}
 
 	public static boolean starts_with_vowel(String word) {
-		boolean  b_low = word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u");
-		boolean  b_up = word.startsWith("A") || word.startsWith("E") || word.startsWith("I") || word.startsWith("O") || word.startsWith("U");
-		return b_low || b_up;
+		// boolean  b_low = word.startsWith("a") || word.startsWith("e") || word.startsWith("i") || word.startsWith("o") || word.startsWith("u");
+		boolean  b_low = word.matches("(a|e|i|o|u).*");
+		return b_low;
 	}
 
+	public static int find_first_vowel(String word) {
+		for (int i = 0 ; i<word.length() ; i++)
+				if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u' || word.charAt(i) == 'y')
+					return i;
+
+		return 1;			
+	}
+				
 	public static String translatePigLatin(String read_word) {
 
 		String[] word_split = read_word.split(" ");
@@ -25,7 +33,8 @@ public class PigLatin {
 			}
 			else
 			{
-				current_word = current_word.substring(1, current_word.length()) + current_word.charAt(0);
+				// current_word = current_word.substring(1, current_word.length()) + current_word.charAt(0);
+				current_word = current_word.substring(find_first_vowel(current_word), current_word.length()) + current_word.substring(0, find_first_vowel(current_word));
 				current_word = current_word + "ay";
 			}
 			tranlated_words =  tranlated_words + current_word + " ";
@@ -48,7 +57,7 @@ public class PigLatin {
 		}
 		else
 		{
-			System.out.println("Please check you input, no number or special character is allowed,;-)");
+			System.out.println("Please check you input, only english letters and space are allowed, ;-)");
 		}
 
     }
