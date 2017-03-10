2e59a8420847073b4573bfd0915638415ca18d7a
cnguyen123 <cnguyen111085@gmail.com>
2017-03-10 08:17:59 +0100

    add translate Piglatin to English

--- src/main/java/PigLatinTranslator/PigLatin.java
+++ src/main/java/PigLatinTranslator/PigLatin.java
@@ -1,15 +1,17 @@
 package PigLatinTranslator;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-
+import java.io.ObjectInputStream.GetField;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.List;
-
 import java.nio.charset.Charset;
 import java.nio.file.*;
 
 public class PigLatin {
-
+	static final char[] VOWELS = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
 	public static boolean isAlpha(String word) {
 		return word.matches("[a-z ,.A-Z]+");
 	}
@@ -27,14 +29,89 @@ public class PigLatin {
 
 		return 1;			
 	}
+			
+	public static List<String> translatePigLatinToEnglish( String pigLatinWord){
+		List<String> englishWord = new ArrayList<String>();
+		//check if input is a pig latin or not
+		if(pigLatinWord.length()<=2){
+			return englishWord;
+		}
+		
+		//check if input is a pig latin of an english word with first character is vowel
+		String suffix = pigLatinWord.substring(pigLatinWord.length()-3, pigLatinWord.length());
+		if(suffix.equals("way") && isVowel((pigLatinWord.charAt(0)))){
+			englishWord.add(pigLatinWord.substring(0, pigLatinWord.length()-3));
+			return englishWord;
+		}
+		
+		//check if input is a pig latin of an english word with first character is consonant
+		suffix = pigLatinWord.substring(pigLatinWord.length()-2, pigLatinWord.length());
+		if(!suffix.equals("ay")){
+			return englishWord;
+		}else{
+			String temp = pigLatinWord.substring(0, pigLatinWord.length()-2);
+			
+			char lastCharacter = temp.charAt(temp.length()-1);
+			char secondLastCharacter =temp.charAt(temp.length() -2);
+			//single consonant
+			if(!isVowel(lastCharacter) && isVowel(secondLastCharacter)){
+				englishWord.add(lastCharacter + temp.substring(0, temp.length() -1));
+			}
+			//maybe english word with consonant cluster, need to check from dictionary for sure
+			else if(!isVowel(lastCharacter) && !isVowel(secondLastCharacter)){
+				//get all possible words from dictionary
+				String word1 = lastCharacter + temp.substring(0, temp.length() -1);
 				
+				String word2 = new StringBuilder().append(secondLastCharacter).append(lastCharacter).append(temp.substring(0, temp.length() -2)).toString();
+				if( isEnglishWord(word1) ){
+					englishWord.add(word1);
+				}
+				if(isEnglishWord(word2)){
+					englishWord.add(word2);
+				}
+			}
+				
+			return englishWord;
+		}
+		
+	}
+	/**
+	 * check whether a word is English word
+	 * @param word
+	 * @return
+	 */
+	public static boolean isEnglishWord(String word){
+		try {
+            BufferedReader in = new BufferedReader(new FileReader(
+                    "edict/google_10000_english.txt"));
+            String str;
+            while ((str = in.readLine()) != null) {
+                if (str.indexOf(word) != -1) {
+                    return true;
+                }
+            }
+            in.close();
+        } catch (IOException e) {
+        	System.out.println("Error reading Dictionary");
+        }
+
+        return false;
+	}
+	public static boolean isVowel(char ch){
+		for (char vowel : VOWELS) {
+	        if (vowel == ch) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
 	public static String translatePigLatin(String read_word) {
 
 		read_word = read_word.replace(",", " ");
 		read_word = read_word.replace(".", " ");
 		read_word = read_word.replace("!", " ");
-		read_word = read_word.replace("“", " ");
-		read_word = read_word.replace("”", " ");
+		read_word = read_word.replace("â€œ", " ");
+		read_word = read_word.replace("â€�", " ");
 		read_word = read_word.replace("\"", " ");
 
 		String[] word_split = read_word.split(" +");
@@ -59,45 +136,84 @@ public class PigLatin {
 		return tranlated_words;
 	}
 
+	public static void commandPanel(){
+		System.out.println("***********************************************");
+		System.out.println("Please choose one of these functions:");
+		System.out.print("Pig Latin --> English(1)|");
+		System.out.print("English --> Pig Latin(2)|");
+		System.out.println("Exit(3)");
+		System.out.println("You choose:");
+		
+	}
     public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Word to be translated:");
-		String read_word = sc.nextLine();
-		if(read_word.contains(".txt"))
-		{
-			try {
-				List<String> lines = Files.readAllLines(Paths.get(read_word), Charset.forName("UTF-8"));
+    	while(true){
+    		commandPanel();
+    		Scanner func = new Scanner(System.in);
+    		String selectedFunc = func.nextLine();
+    		if(selectedFunc.equals("1")){
+    			Scanner sc = new Scanner(System.in);
+    			System.out.println("Word to be translated:");
+    			String read_word = sc.nextLine();
+    			if(read_word.contains(".txt"))
+    			{
+    				try {
+    					List<String> lines = Files.readAllLines(Paths.get(read_word), Charset.forName("UTF-8"));
 
-				System.out.println("Found input file");
-				for (String line : lines) {
+    					System.out.println("Found input file");
+    					for (String line : lines) {
 
-					System.out.println("Word to be translated:");
-					System.out.println(line);
+    						System.out.println("Word to be translated:");
+    						System.out.println(line);
 
-					if (line.matches(".*\\w.*"))
-					{
-						System.out.println("Translated in Pig Latin:");
-						System.out.println(translatePigLatin(line));
-					}
-			}
-			} catch (IOException e) {
-				System.out.println(e);
-			}
+    						if (line.matches(".*\\w.*"))
+    						{
+    							System.out.println("Translated in Pig Latin:");
+    							System.out.println(translatePigLatin(line));
+    						}
+    				}
+    				} catch (IOException e) {
+    					System.out.println(e);
+    				}
 
 
-		}
-		else{
-			if(isAlpha(read_word))
-			{
-				System.out.println("Translated in Pig Latin:");
-				System.out.println(translatePigLatin(read_word));
-			}
-			else
-			{
-				System.out.println("Please check you input, only english letters and space are allowed, ;-)");
-			}
-		}
+    			}
+    			else{
+    				if(isAlpha(read_word))
+    				{
+    					System.out.println("Translated in Pig Latin:");
+    					System.out.println(translatePigLatin(read_word));
+    				}
+    				else
+    				{
+    					System.out.println("Please check you input, only english letters and space are allowed, ;-)");
+    				}
+    			}
+    			
+    		}else if(selectedFunc.equals("2")){
+    			Scanner sc = new Scanner(System.in);
+    			System.out.println("Word to be translated:");
+    			String inputWord = sc.nextLine();
+    			System.out.println("Translate from Pig Latin to English:");
+				List<String> result = translatePigLatinToEnglish(inputWord);
+    			if(result.size()==0){
+    				System.out.println("Input invalid Piglatin, please try again!!");
+    			}
+    			else{
+    				
+    			for(int i = 0; i <result.size(); i++){
+    				System.out.println(result.get(i));
+    			}
+    			}
+    		}else if(selectedFunc.equals("3")){
+    			System.out.println("YEBAY!!!");
+    			System.exit(1);
+    		}else {
+    			System.out.println("Type 1 or 2 or 3 please!!!");
+    		}
+    		
+    	}
+		
 		
     }
 }
