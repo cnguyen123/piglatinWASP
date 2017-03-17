64793a111a14ccf11241a5d3a6441557756a3ea3
Batkow <ivobatkovic@gmail.com>
2017-03-17 16:24:46 +0100

    Replaced the firstvowel method with a cleaner code

--- src/main/java/PigLatinTranslator/PigLatin.java
+++ src/main/java/PigLatinTranslator/PigLatin.java
@@ -43,8 +43,11 @@ public class PigLatin {
 
 	public static int findFirstVowel(String word) {
 		for (int i = 0 ; i<word.length() ; i++)
-				if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u' || word.charAt(i) == 'y')
+				
+				// Vowel check
+				if ("AEIOUaeiou".indexOf(word.charAt(i)) != -1) {
 					return i;
+				}
 
 		return 1;			
 	}
